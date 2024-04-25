f614ddbf752124535a708c87d640593a1b4d1547
services-reworks <167835915+services-reworks@users.noreply.github.com>
2024-04-25 19:25:16 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
 function test_get() {
-    //const url = "https://script.googleusercontent.com/macros/echo?user_content_key=pBzHvr1eVKfsa0ajdvwNVfGj6p6kx473zwZrvV-eVXb5G230LZCYE887-MuzXC7OAQMyPL_TV8BGWG2vaBMT77m0Au-xb_-mm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCjgmyJ5mTmYkXbTOa_NV6RRTf4_J4x6viySO3J2JF7TgUscvus5zYOPpehwqDtynQ&lib=MVsEhW8P4pJs-UzBi9Xnlq3KRT9d30hqB";
-    //const url = "https://script.google.com/macros/s/AKfycbyyEc6z2xQ5KIxnR1hYb_EknOT6ltN-tyImawiOfUXwIOHML0wRn3pu1Lx6UUyolvpu/exec";
 
-    //v2   
-    const url = "https://script.google.com/macros/s/AKfycbwZT026a99uoqSRtRpaA4I3GxIo-6bl26ORMOmmkk85fzfN40Nv4EzVm1GsX_pbN7PS/exec";
-    const url_dev = "https://script.google.com/macros/s/AKfycby-qpy3QomwT-C6Vqf9Dn1k_diPepfPLUYqX1SmMvY/dev";
-    /*
-        fetch(url,
-            {
-                redirect: "follow",
-                mode: "no-cors",
-                method: "GET",
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            })
-            .then(d => d.json())
-            .then(d => {
-                document.getElementById("app").textContent = d[0].status;
-            });
-    */
-
+    const url = "https://script.google.com/macros/s/AKfycbzSOZKBKWzaEjVORtSnZBy6PD2OarMVfOZpKrK3EwczJQl59By6ECvG8csxivA4c6Z9/exec"
 
     fetch(url)
         .then(d => d.json())
         .then(d => {
             document.getElementById("app").textContent = d[0].status;
         });
+    //-- done.
+}
 
+
+function addrow() {
+
+    const url = "https://script.google.com/macros/s/AKfycbzSOZKBKWzaEjVORtSnZBy6PD2OarMVfOZpKrK3EwczJQl59By6ECvG8csxivA4c6Z9/exec"
+
+    fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "no-cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+            "Content-Type": "application/json",
+        },
+        redirect: "follow", // manual, *follow, error
+        //referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify( {name: "Olivia"} ), // body data type must match "Content-Type" header
+    });
 
     //-- done.
 }
 
 document.getElementById("btn").addEventListener("click", test_get);
+document.getElementById("btn2").addEventListener("click", addrow);
